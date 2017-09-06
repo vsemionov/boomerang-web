5f881eeddbe50e18f7414e3921acbb14e6281701
Victor Semionov <vsemionov@gmail.com>
2017-09-06 10:36:40 +0300

    error reporting with raven/sentry

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+
 import App from './app/app.vue';
+
+
+Raven
+    .config('https://1fb80c1e5ca04837bc8d4354c600fa9c@sentry.io/213281')
+    .addPlugin(RavenVue, Vue)
+    .install();
+
 
 Vue.use(VueRouter);
 
+
 const app = new Vue({
-  el: 'app',
-  render: h => h(App)
+    el: 'app',
+    render: h => h(App)
 });
