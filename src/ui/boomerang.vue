e97937cc75ab2fb81ea43d31e7c94d1f80ba0971
Victor Semionov <vsemionov@gmail.com>
2017-09-09 15:38:16 +0300

    separate navbar component

--- src/ui/boomerang.vue
+++ src/ui/boomerang.vue
@@ -1,29 +1,6 @@
 <template>
     <div class="wrapper">
-        <nav class="navbar navbar-static-top navbar-inverse">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class='navbar-brand' href='/'>Boomerang</a>
-                </div>
-
-                <ul class="nav navbar-nav pull-right">
-                    <template v-if="!isLoggedIn()">
-                        <li><router-link to="/login"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Log In</router-link></li>
-                        <li><router-link to="/signup"><span class="glyphicon glyphicon-check"></span>&nbsp;Sign Up</router-link></li>
-                    </template>
-
-                    <template v-else>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;vsemionov<span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><router-link to="/settings"><span class="glyphicon glyphicon-cog"></span>&nbsp;Settings</router-link></li>
-                                <li><router-link to="/logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log Out</router-link></li>
-                            </ul>
-                        </li>
-                    </template>
-                </ul>
-            </div>
-        </nav>
+        <navbar></navbar>
 
         <div class="container">
             <h1>Boomerang</h1>
@@ -34,14 +11,10 @@
 
 
 <script>
-    import { isLoggedIn } from '../auth.js';
+    import Navbar from './navbar.vue';
 
     export default {
         name: 'boomerang',
-        methods: {
-            isLoggedIn() {
-                return isLoggedIn();
-            }
-        }
+        components: { Navbar }
     };
 </script>
---
+++ src/ui/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar navbar-static-top navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <a class='navbar-brand' href='/'>Boomerang</a>
+            </div>
+
+            <ul class="nav navbar-nav pull-right">
+                <template v-if="!isLoggedIn()">
+                    <li><router-link to="/login"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Log In</router-link></li>
+                    <li><router-link to="/signup"><span class="glyphicon glyphicon-check"></span>&nbsp;Sign Up</router-link></li>
+                </template>
+
+                <template v-else>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;vsemionov<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><router-link to="/settings"><span class="glyphicon glyphicon-cog"></span>&nbsp;Settings</router-link></li>
+                            <li><router-link to="/logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log Out</router-link></li>
+                        </ul>
+                    </li>
+                </template>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+    import { isLoggedIn } from '../auth.js';
+
+    export default {
+        name: 'navbar',
+        methods: {
+            isLoggedIn() {
+                return isLoggedIn();
+            }
+        }
+    };
+</script>
