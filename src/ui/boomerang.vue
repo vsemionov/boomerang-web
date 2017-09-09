96e771dfb3b679454f0d40eb95cc3cd3d96233ad
Victor Semionov <vsemionov@gmail.com>
2017-09-09 03:35:02 +0300

    conditional profile link rendering

--- src/ui/boomerang.vue
+++ src/ui/boomerang.vue
@@ -7,16 +7,20 @@
                 </div>
 
                 <ul class="nav navbar-nav pull-right">
-                    <li><router-link to="/login"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Log In</router-link></li>
-                    <li><router-link to="/signup"><span class="glyphicon glyphicon-check"></span>&nbsp;Sign Up</router-link></li>
+                    <template v-if="!isLoggedIn()">
+                        <li><router-link to="/login"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Log In</router-link></li>
+                        <li><router-link to="/signup"><span class="glyphicon glyphicon-check"></span>&nbsp;Sign Up</router-link></li>
+                    </template>
 
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;vsemionov<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><router-link to="/settings"><span class="glyphicon glyphicon-cog"></span>&nbsp;Settings</router-link></li>
-                            <li><router-link to="/logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log Out</router-link></li>
-                        </ul>
-                    </li>
+                    <template v-else>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;vsemionov<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><router-link to="/settings"><span class="glyphicon glyphicon-cog"></span>&nbsp;Settings</router-link></li>
+                                <li><router-link to="/logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log Out</router-link></li>
+                            </ul>
+                        </li>
+                    </template>
                 </ul>
             </div>
         </nav>
@@ -30,7 +34,14 @@
 
 
 <script>
+    import { isLoggedIn } from '../auth.js';
+
     export default {
-        name: 'boomerang'
+        name: 'boomerang',
+        methods: {
+            isLoggedIn() {
+                return isLoggedIn();
+            }
+        }
     };
 </script>
