8c869c94c45689095f7d9162befa31895ec3ba18
Victor Semionov <vsemionov@gmail.com>
2017-09-09 21:06:22 +0300

    update auth state from jwt token

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,31 @@
+import decode from 'jwt-decode';
+
+const JWT_KEY = 'jwt';
+
 export const authState = {
     username: null
 };
 
+
+function isTokenExpired(token) {
+  const expirationDate = getTokenExpirationDate(token);
+  return expirationDate < new Date();
+}
+
 export function updateAuthState() {
-    authState.username = 'vsemionov';
+    let username = null;
+
+    const token = localStorage.getItem(JWT_KEY);
+    if (token) {
+        const payload = decode(token);
+        if (payload.exp) {
+            if (payload.exp * 1000 > Date.now()) {
+                if (payload.username) {
+                    username = payload.username;
+                }
+            }
+        }
+    }
+
+    authState.username = username;
 }
