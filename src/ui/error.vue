d07dde5e83c5ab7e317ddf7ecd43868da9f78365
Victor Semionov <vsemionov@gmail.com>
2017-09-11 23:03:45 +0300

    known error for status 409 conflict

--- src/ui/error.vue
+++ src/ui/error.vue
@@ -6,6 +6,7 @@
             <p v-if="error.response.status == 404">Not found</p>
             <p v-else-if="error.response.status == 401">Unauthorized</p>
             <p v-else-if="error.response.status == 403">Forbidden</p>
+            <p v-else-if="error.response.status == 409">Conflict</p>
             <p v-else>Unknown error ({{error.response.status}})</p>
         </template>
 
