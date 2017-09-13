7fa1e250a8b5c74a5149be97334df8fa69ad8f1c
Victor Semionov <vsemionov@gmail.com>
2017-09-13 04:45:29 +0300

    error details

--- src/ui/error.vue
+++ src/ui/error.vue
@@ -3,14 +3,20 @@
         <h2>Error</h2>
 
         <template v-if="error.response">
-            <p v-if="error.response.status == 404">Not found</p>
-            <p v-else-if="error.response.status == 401">Unauthorized</p>
-            <p v-else-if="error.response.status == 403">Forbidden</p>
-            <p v-else-if="error.response.status == 409">Conflict</p>
-            <p v-else>Unknown error ({{ error.response.status }})</p>
+            <h3 v-if="error.response.status == 404">Not Found</h3>
+            <h3 v-else-if="error.response.status == 401">Unauthorized</h3>
+            <h3 v-else-if="error.response.status == 403">Forbidden</h3>
+            <h3 v-else-if="error.response.status == 402">Payment Required</h3>
+            <h3 v-else-if="error.response.status == 409">Conflict</h3>
+            <h3 v-else>Unknown error ({{ error.response.status }})</h3>
+
+            <h4 v-if="error.response.data">{{ error.response.data.detail }}</h4>
         </template>
 
-        <p v-else>Communication error</p>
+        <template v-else>
+            <h3>Communication error</h3>
+            <h4>{{ error.message }}</h4>
+        </template>
     </div>
 </template>
 
