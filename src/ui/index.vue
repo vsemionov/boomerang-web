5c04ac4e98f35f4b200696141449bf7ef43a0bdc
Victor Semionov <vsemionov@gmail.com>
2017-09-15 06:52:03 +0300

    indent buttons welcome message

--- src/ui/index.vue
+++ src/ui/index.vue
@@ -4,13 +4,20 @@
 
         <template v-if="authState.username">
             <br/>
-            <div class="row">
-                <router-link :to="{ name: 'notebooks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Notebooks</router-link>
+            <div class="container">
+                <div class="row">
+                    <router-link :to="{ name: 'notebooks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Notebooks</router-link>
+                </div>
+                <p></p>
+                <div class="row">
+                    <router-link :to="{ name: 'tasks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Tasks</router-link>
+                </div>
             </div>
-            <p></p>
-            <div class="row">
-                <router-link :to="{ name: 'tasks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Tasks</router-link>
-            </div>
+        </template>
+
+        <template v-else>
+            <p>Welcome!</p>
+            <p>Please log in or sign up.</p>
         </template>
     </div>
 </template>
