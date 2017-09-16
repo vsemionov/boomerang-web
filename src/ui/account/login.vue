b152a4a75978931dd9de3b27d369ede16d6c7ebb
Victor Semionov <vsemionov@gmail.com>
2017-09-16 18:07:10 +0300

    ported login

--- src/ui/account/login.vue
+++ src/ui/account/login.vue
@@ -4,26 +4,27 @@
 
         <br/>
 
-        <a class="btn btn-social btn-facebook" :href="getSocialLoginUrl('facebook')"><span class="fa fa-facebook"></span>Log in with Facebook</a>
+        <b-row align-h="center">
+            <b-col cols="10" sm="10" md="8" lg="6" xl="5">
+                <b-button block class="btn-social btn-facebook" :href="getSocialLoginUrl('facebook')"><span class="fa fa-facebook"></span>Log in with Facebook</b-button>
 
-        <p></p>
+                <b-button block class="btn-social btn-google" :href="getSocialLoginUrl('google')"><span class="fa fa-google"></span>Log in with Google</b-button>
 
-        <a class="btn btn-social btn-google" :href="getSocialLoginUrl('google')"><span class="fa fa-google"></span>Log in with Google</a>
+                <br/>
 
-        <p></p>
-        <br/>
+                <b-form @submit.prevent="authenticate">
+                    <b-form-group label="Username or email" label-for="username">
+                        <b-form-input type="text" id="username" ref="username" v-model="username" placeholder="Username or email" required autofocus></b-form-input>
+                    </b-form-group>
 
-        <form class="form-signin" @submit.prevent="authenticate">
-            <div class="form-group">
-                <label for="username">Username or email</label>
-                <input type="text" id="username" v-model="username" ref="username" class="form-control" placeholder="Username or email" required autofocus>
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="password" class="form-control" placeholder="Password" required>
-            </div>
-          <button type="submit" class="btn btn-primary">Log in</button>
-        </form>
+                    <b-form-group label="Password" label-for="password">
+                        <b-form-input type="password" id="password" v-model="password" placeholder="Password" required></b-form-input>
+                    </b-form-group>
+
+                    <b-button type="submit" variant="primary">Log in</b-button>
+                </b-form>
+            </b-col>
+        </b-row>
 
         <spinner v-if="working"></spinner>
 
--- src/ui/index.vue
+++ src/ui/index.vue
@@ -5,17 +5,21 @@
         <br/>
 
         <template v-if="authState.username">
+
             <b-row>
                 <b-col cols="12" sm="12" md="6" lg="4" xl="3">
                     <b-button :to="{ name: 'notebooks', params: { username: authState.username } }" size="lg" block>Notebooks</b-button>
                 </b-col>
             </b-row>
+
             <p></p>
+
             <b-row>
                 <b-col cols="12" sm="12" md="6" lg="4" xl="3">
                     <b-button :to="{ name: 'tasks', params: { username: authState.username } }" size="lg" block>Tasks</b-button>
                 </b-col>
             </b-row>
+
         </template>
 
         <template v-else>
