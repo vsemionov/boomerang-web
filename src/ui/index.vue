df4002cb61468a0aafae43aeea04fb7475d21fb8
Victor Semionov <vsemionov@gmail.com>
2017-09-16 12:06:45 +0300

    ported home page

--- src/ui/index.vue
+++ src/ui/index.vue
@@ -2,17 +2,20 @@
     <div>
         <h1>Home</h1>
 
+        <br/>
+
         <template v-if="authState.username">
-            <br/>
-            <div class="container">
-                <div class="row">
-                    <router-link :to="{ name: 'notebooks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Notebooks</router-link>
-                </div>
-                <p></p>
-                <div class="row">
-                    <router-link :to="{ name: 'tasks', params: { username: authState.username } }" class="btn btn-primary btn-lg col-xs-12 col-sm-6 col-md-4 col-lg-3" role="button">Tasks</router-link>
-                </div>
-            </div>
+            <b-row>
+                <b-col cols="12" sm="12" md="6" lg="4" xl="3">
+                    <b-button :to="{ name: 'notebooks', params: { username: authState.username } }" size="lg" block>Notebooks</b-button>
+                </b-col>
+            </b-row>
+            <p></p>
+            <b-row>
+                <b-col cols="12" sm="12" md="6" lg="4" xl="3">
+                    <b-button :to="{ name: 'tasks', params: { username: authState.username } }" size="lg" block>Tasks</b-button>
+                </b-col>
+            </b-row>
         </template>
 
         <template v-else>
