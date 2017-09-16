ff436365b340e78442be22aa1e853df4cfb7c2e5
Victor Semionov <vsemionov@gmail.com>
2017-09-16 11:25:22 +0300

    reordered font extensions

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(png|jpg|gif|svg|woff|woff2|ttf|eot)$/,
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]?[hash]'
