a388ff23cab72db7e69bfd99154eeecf975ecd2d
Victor Semionov <vsemionov@gmail.com>
2017-09-22 16:06:47 +0300

    use an absolute position to center the spinner on the screen

--- src/ui/spinner.vue
+++ src/ui/spinner.vue
@@ -12,12 +12,16 @@
 
 <style>
     .spinner {
-        margin: auto;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        z-index: 1;
         width: 64px;
         height: 64px;
-        border: 6px solid rgba(0, 174, 239, .15);
-        border-top: 6px solid rgba(0, 174, 239, .8);
+        margin: -32px 0 0 -32px;
         border-radius: 50%;
+        border: 6px solid rgba(192, 192, 192, .25);
+        border-top: 6px solid rgba(0, 153, 255, 1);
         animation: rotation 1s infinite linear;
     }
 
