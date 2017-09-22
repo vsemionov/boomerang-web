a6099277da82d04b73e5a6a4db611dcc94624c2a
Victor Semionov <vsemionov@gmail.com>
2017-09-22 15:48:53 +0300

    corrected indentation

--- src/ui/spinner.vue
+++ src/ui/spinner.vue
@@ -12,17 +12,17 @@
 
 <style>
     .spinner {
-       margin: auto;
-       width: 64px;
-       height: 64px;
-       border: 6px solid rgba(0, 174, 239, .15);
-       border-top: 6px solid rgba(0, 174, 239, .8);
-       border-radius: 50%;
-       animation: rotation 1s infinite linear;
+        margin: auto;
+        width: 64px;
+        height: 64px;
+        border: 6px solid rgba(0, 174, 239, .15);
+        border-top: 6px solid rgba(0, 174, 239, .8);
+        border-radius: 50%;
+        animation: rotation 1s infinite linear;
     }
 
     @keyframes rotation {
-       from { transform: rotate(0deg); }
-       to { transform: rotate(360deg); }
+        from { transform: rotate(0deg); }
+        to { transform: rotate(360deg); }
     }
 </style>
