133ee9372fd4902e925f3a799b601d7f640613c0
Victor Semionov <vsemionov@gmail.com>
2017-09-22 20:03:11 +0300

    use raven/sentry only on production

--- src/main.js
+++ src/main.js
@@ -16,17 +16,21 @@ import 'bootstrap-social/bootstrap-social.css';
 import { version } from '../package.json';
 import { API_URL } from './urls.js';
 import { router } from './router.js';
+
 import Boomerang from './ui/boomerang.vue';
+
 import './ui/boomerang.css';
 
 
-Raven
-    .config('https://1fb80c1e5ca04837bc8d4354c600fa9c@sentry.io/213281', {
-        release: version,
-        environment: process.env.NODE_ENV
-    })
-    .addPlugin(RavenVue, Vue)
-    .install();
+if (process.env.NODE_ENV === 'production') {
+    Raven
+        .config('https://1fb80c1e5ca04837bc8d4354c600fa9c@sentry.io/213281', {
+            release: version,
+            environment: process.env.NODE_ENV
+        })
+        .addPlugin(RavenVue, Vue)
+        .install();
+}
 
 
 axios.defaults.baseURL = API_URL;
